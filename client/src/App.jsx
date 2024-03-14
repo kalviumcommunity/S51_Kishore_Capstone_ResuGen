898e8a8c0a818aab19304571f1e057d24be30018
Kishore <ki225ku@gmail.com>
2024-03-14 13:48:00 +0530

    added navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,19 @@
 import './App.css'
 import LandingPage from './component/LandingPage'
+import LoginPage from "./component/Login/LoginPage"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
 
   return (
     <>
-        {/* <LandingPage /> */}
-        Created FrontEnd
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path='/login' element={<LoginPage />} /> 
+      </Routes>
+    </BrowserRouter>
+        
     </>
   )
 }
