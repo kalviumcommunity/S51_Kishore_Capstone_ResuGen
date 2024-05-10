30d3dbe99fe43aa87cf40323623e8a2e84d5859e
Kishore <ki225ku@gmail.com>
2024-05-10 15:01:14 +0530

    gemini backend done

--- Backend/model/Template.model.js
+++ Backend/model/Template.model.js
@@ -1,12 +1,12 @@
-    const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-    const templateSchema = new mongoose.Schema({
-        templateID: {
-            type: Number,
-        },
-        templateImg: {
-            type: String,
-        }
-    })
+const templateSchema = new mongoose.Schema({
+  templateID: {
+    type: Number,
+  },
+  templateImg: {
+    type: String,
+  },
+});
 
-    module.exports = mongoose.model("templates", templateSchema)
+module.exports = mongoose.model("templates", templateSchema);
